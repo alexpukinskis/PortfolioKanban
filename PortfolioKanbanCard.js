b4fa8c08d891d4e10f09df93a71fbabfdb9af48a
Jim Campbell <jcampbell@rallydev.com>
2012-06-19 15:30:42 -0600

    Mild method refactoring, start of supporting User Stories

--- PortfolioKanbanCard.js
+++ PortfolioKanbanCard.js
@@ -83,18 +83,20 @@
 				var directChildrenCount = this.getRecord().get('DirectChildrenCount');
 				
 				if (directChildrenCount > 0) {  
-				
-					var children = this.getRecord().get('Children');
-				
+					
+					this.children = this.getRecord().get('Children');
+					if (this.children.length != directChildrenCount) {
+						this.children = this.getRecord().get('UserStories');
+					} 
 					output.add({
 		                xtype:'component',
 		                cls:'numberOfChildren',
 						itemId:'childrenCountContainer',
-		                html: this._buildCountLabel(children, directChildrenCount),
+		                html: this._buildCountLabel(directChildrenCount),
 						listeners: {
 					        click: {
 					        	element: 'el',
-					        	fn: Ext.bind(this._toggleChildrenVisibility, this, [children])
+					        	fn: Ext.bind(this._toggleChildrenVisibility, this)
 					         },
 					         scope: this
 					     }
@@ -109,12 +111,12 @@
 				return output;
 		},
 		
-		_buildCountLabel:function(children, directChildrenCount) {
-		    if (children.length > 0) { //then the children are also PIs
-    			var childrenType = children[0].PortfolioItemType._refObjectName;
-    			return Ext.util.Format.plural(children.length, childrenType);
+		_buildCountLabel:function(directChildrenCount) {
+		    if (this.children.length > 0) { //then the children are also PIs
+    			var childrenType = this.children[0].PortfolioItemType._refObjectName;
+    			return Ext.util.Format.plural(this.children.length, childrenType);
     		} else { //the children are user stories
-    		    if (children.length == 1) {
+    		    if (this.children.length == 1) {
     		        return countLabel = '1 user story';
     		    } else {
     			    return countLabel = directChildrenCount + ' user stories';    
@@ -125,22 +127,29 @@
 		
 		// when label is clicked here is where the work is done to build and display or turn off the child card list
 		_toggleChildrenVisibility:function(children) {
-			var childrenContainer = this.down('#childrenContainer')
-			var expanded = childrenContainer.down('.component');
+			this.childrenContainer = this.down('#childrenContainer')
+			var expanded = this.childrenContainer.down('.component');
 			if (expanded) {
-				childrenContainer.removeAll();
+				this.childrenContainer.removeAll();
 				return;
 			}
-				
-			for (var i = 0; i < children.length; i++) {
-				var child = children[i];
+			
+			this._renderChildren();
+
+		},
+		
+		
+		// 
+		_renderChildren:function() {
+				for (var i = 0; i < this.children.length; i++) {
+				var child = this.children[i];
 			    if (child.State) {
 					var state = child.State._refObjectName;
 				} else {
 					var state = 'not on board';
 				}
 				var childURL = Rally.util.Navigation.createRallyDetailUrl(child);
-				childrenContainer.add({
+				this.childrenContainer.add({
 	                xtype:'component',
 	                cls:'childlabel',
 	                renderTpl: '<a href="{childURL}" target="_top">{childName}</a> ({childState})<br>',
@@ -151,9 +160,7 @@
 					}
 	            });		
 
-			}
-		}
-
+	    }}
 
     });
 
